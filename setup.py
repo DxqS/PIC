0cc80d2320de874237a5b4b85e96b36d144514bb
Dxq <353622088@qq.com>
2017-05-03 21:41:34 +0800

    PIC

---
+++ setup.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+from setuptools import find_packages, setup
+
+setup(
+    name='PIC',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=['pillow'],
+    url='https://github.com/DxqS/PIC',
+    license='MIT',
+    author='Dxq',
+    author_email='chk0125@126.com',
+    description='图片合成 API',
+    keywords=[
+        'PIL',
+        'pillow',
+        'merge'
+    ]
+)
+
